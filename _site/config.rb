ec06dfd246a74c28eb79aaaafdc791e71e900948
kmweeks <kevinmichaelweeks@gmail.com>
2013-10-21 22:39:24 -0700

    Adding autoprefixer, fixing space below portrait

--- _site/config.rb
+++ _site/config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require 'autoprefixer-rails'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -8,18 +9,25 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
 output_style = :expanded
+# or :nested or :compact or :compressed
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# Autoprefixer
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << AutoprefixerRails.compile(css)
+  end
+end

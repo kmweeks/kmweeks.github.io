f7d6a48bed578242d7baedddce6f7268a594facb
kmweeks <kevinmichaelweeks@gmail.com>
2013-10-28 22:22:23 -0700

    Minified css, losslessly compressed background image

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
 require 'autoprefixer-rails'
+require 'csso'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -10,13 +11,13 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
-# or :nested or :compact or :compressed
+# :expanded, :nested, :compact, or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+#line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
@@ -25,9 +26,10 @@ relative_assets = true
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
 # Autoprefixer
+# remove Csso.optimize() to stop minification
 on_stylesheet_saved do |file|
   css = File.read(file)
   File.open(file, 'w') do |io|
-    io << AutoprefixerRails.compile(css)
+    io << Csso.optimize( AutoprefixerRails.compile(css) )
   end
 end

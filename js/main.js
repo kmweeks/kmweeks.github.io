3274a1c686d514de2b45c43d70482c37054db497
kmweeks <kevinmichaelweeks@gmail.com>
2014-02-14 01:35:02 -0800

    Styling site, adding topics page functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Slide In Menu
 (function () {
   var $container = document.getElementById('container'),
       $menuTrigger = document.getElementById('menu-trigger'),
@@ -9,9 +10,9 @@
     close($container);
   });
 
-  $menuTrigger.addEventListener('click', function (event) {
+  $menuTrigger.addEventListener('click', function (e) {
 
-    event.stopPropagation();
+    e.stopPropagation();
 
     if (!revealed) {
       reveal($container);
@@ -21,8 +22,8 @@
 
   }, false);
 
-  $menu.addEventListener('click', function (event) {
-    event.stopPropagation();
+  $menu.addEventListener('click', function (e) {
+    e.stopPropagation();
   });
 
   // Trigger slideout
@@ -36,5 +37,27 @@
     elem.classList.remove('menu-open');
     revealed = false;
   };
+
+})();
+
+// Topics List
+(function() {
+  var $topic =  $('[data-topic]'),
+      $topicFilter = $('[data-topic-filter]');
+
+  $topic.addClass('is-hidden');
+
+  $topicFilter.on('click', function(e) {
+    e.preventDefault();
+
+    var id = $(this).attr("href");
+    id = String(id);
+
+    $topic.addClass('is-hidden');
+    $(id).removeClass('is-hidden');
+
+    $topicFilter.removeClass('is-current');
+    $(this).addClass('is-current');
+  });
 
 })();
